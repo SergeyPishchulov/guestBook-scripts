7d7a5e3c35df2bc507ed9d748247025714b23920
SergeyPishchulov <lcmrb26@bk.ru>
2023-01-15 16:32:33 +0500

    disable question to user

--- create_and_add_vm.ps1
+++ create_and_add_vm.ps1
@@ -5,7 +5,7 @@ $vmexternaladdress = $machine.network_interfaces[0].primary_v4_address.one_to_on
 yc load-balancer target-group add-targets --name guest-vm-tg --target address=$vmaddress,subnet-name=default-ru-central1-a
 
 Start-Sleep -Seconds 20
-ssh yc-user@$vmexternaladdress "sudo apt-get update;sudo apt install git -y;git clone https://github.com/SergeyPishchulov/guestBook-backend;cd guestBook-backend;sudo apt-get install uvicorn -y;sudo apt install python3-pip -y;sudo pip install fastapi;sudo pip install -r requirements.txt;sudo ufw allow 80; sudo uvicorn main:app --host 0.0.0.0 --port 80"
+ssh -o "StrictHostKeyChecking no" yc-user@$vmexternaladdress "sudo apt-get update;sudo apt install git -y;git clone https://github.com/SergeyPishchulov/guestBook-backend;cd guestBook-backend;sudo apt-get install uvicorn -y;sudo apt install python3-pip -y;sudo pip install fastapi;sudo pip install -r requirements.txt;sudo ufw allow 80; sudo uvicorn main:app --host 0.0.0.0 --port 80"
 
 
 
